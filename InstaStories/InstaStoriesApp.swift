d0ac740404a3ca779d14429f101d5cdaad8f4083
Kévin Esprit <kevinesprit@icloud.com>
2025-03-20 14:10:23 +0100

    Initial Commit

---
+++ InstaStories/InstaStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaStoriesApp.swift
+//  InstaStories
+//
+//  Created by Kévin Esprit on 20/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstaStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaStoriesTests/InstaStoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstaStoriesTests.swift
+//  InstaStoriesTests
+//
+//  Created by Kévin Esprit on 20/03/2025.
+//
+
+import Testing
+@testable import InstaStories
+
+struct InstaStoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
